db9ffe138b2a4ddf99ec0aaf1c629f38711d8955
Tiago Marques <tiago.marques@hpe.com>
2016-10-20 17:47:17 +0200

    API Polishing

--- src/main/java/com/thirdparty/EverythingParser.java
+++ src/main/java/com/thirdparty/EverythingParser.java
@@ -3,13 +3,15 @@ package com.thirdparty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.fortify.plugin.event.ScanParse;
-import com.fortify.plugin.parser.ParserPlugin;
-import com.fortify.plugin.queue.ResultQueue;
 import com.fortify.plugin.result.Vulnerability;
+import com.fortify.plugin.spi.ParserPlugin;
+import com.fortify.plugin.spi.VulnerabilityPublisher;
 import com.thirdparty.scan.Finding;
 import com.thirdparty.scan.Scan;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.ServiceLoader;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -19,6 +21,16 @@ import java.util.concurrent.Executors;
 public class EverythingParser implements ParserPlugin {
     
     private static final ObjectMapper JSONMAPPER = new ObjectMapper();
+    private final VulnerabilityPublisher publisher;
+    
+    public EverythingParser() {
+        ServiceLoader<VulnerabilityPublisher> loader = ServiceLoader.load(VulnerabilityPublisher.class);
+        Iterator<VulnerabilityPublisher> implementations = loader.iterator();
+        publisher = implementations.next();
+        if (publisher == null || implementations.hasNext()) {
+            throw new IllegalStateException("None or multiple implementations found for " + ParserPlugin.class.getCanonicalName());
+        }
+    }
     
     private final ExecutorService executor = Executors.newSingleThreadExecutor();
     @Override
@@ -28,7 +40,8 @@ public class EverythingParser implements ParserPlugin {
     }
 
     @Override
-    public boolean accept(ScanParse event, ResultQueue<Vulnerability> pipe) {
+    public boolean accept(ScanParse event) {
+        System.out.println("Received file " +  event.getFileName() + " scan id " + event.getScanId());
         ObjectReader r = JSONMAPPER.readerFor(Scan.class);
         try {
             final Scan s = r.readValue(event.getContent());
@@ -45,21 +58,19 @@ public class EverythingParser implements ParserPlugin {
                             v.setField(f.getField());
                             v.setInstanceId(f.getUniqueId());
                             v.setScanId(event.getScanId());
-                            pipe.add(v);
+                            publisher.publish(v);
                         }
+                        System.out.println("Processed " + event.getScanId());
                     } catch (InterruptedException e) {
                         System.out.println("Stopped!");
                         return;
                     }
     
-                    System.out.println("Received file " +  event.getFileName() + " with id " + event.getScanId());
-    
-                    pipe.published();
                 });
                 return true;
             }
         } catch (IOException  e) {
-            System.err.println("FAILED with EX");
+            System.err.println("FAILED because of my EX");
             return false;
         } finally {
             try {
