0a90ce262d7c96bfff377437e293d2609c27d3a8
tomas.potocky <tomas.potocky@hp.com>
2016-11-16 18:43:15 +0100

    Plugin API modifications, only one publisher acepting either Event or Result

--- src/main/java/com/thirdparty/EverythingParser.java
+++ src/main/java/com/thirdparty/EverythingParser.java
@@ -2,10 +2,12 @@ package com.thirdparty;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
+import com.fortify.plugin.PluginPublisher;
+import com.fortify.plugin.event.Event;
 import com.fortify.plugin.event.ScanSubmission;
+import com.fortify.plugin.event.parser.AcceptScan;
 import com.fortify.plugin.result.Vulnerability;
 import com.fortify.plugin.spi.ParserPlugin;
-import com.fortify.plugin.spi.VulnerabilityPublisher;
 import com.thirdparty.scan.Finding;
 import com.thirdparty.scan.Scan;
 import org.slf4j.Logger;
@@ -14,8 +16,7 @@ import org.slf4j.LoggerFactory;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Iterator;
-import java.util.ServiceLoader;
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -24,18 +25,9 @@ import java.util.concurrent.Executors;
  */
 public class EverythingParser implements ParserPlugin {
     private static final Logger LOG = LoggerFactory.getLogger(EverythingParser.class);
+    private PluginPublisher publisher;
 
     private static final ObjectMapper JSONMAPPER = new ObjectMapper();
-    private final VulnerabilityPublisher publisher;
-
-    public EverythingParser() {
-        ServiceLoader<VulnerabilityPublisher> loader = ServiceLoader.load(VulnerabilityPublisher.class, EverythingParser.class.getClassLoader());
-        Iterator<VulnerabilityPublisher> implementations = loader.iterator();
-        publisher = implementations.next();
-        if (publisher == null || implementations.hasNext()) {
-            throw new IllegalStateException("None or multiple implementations found for " + ParserPlugin.class.getCanonicalName());
-        }
-    }
 
     private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
@@ -46,45 +38,60 @@ public class EverythingParser implements ParserPlugin {
     }
 
     @Override
-    public boolean accept(ScanSubmission event) {
-        ObjectReader r = JSONMAPPER.readerFor(Scan.class);
-        final String location = event.getLocation();
-        LOG.info("Reading stream from location:" + location);
+    public void handle(Event event) {
+        if (event instanceof ScanSubmission) {
+            ScanSubmission scanSubmission = (ScanSubmission) event;
+            ObjectReader r = JSONMAPPER.readerFor(Scan.class);
+            final String location = scanSubmission.getLocation();
+            LOG.info("Reading stream from location:" + location);
 
-        // Simulation of location data stream
-        InputStream content = new ByteArrayInputStream(new String("{ \"findings\" : [{ \"uniqueId\": 0, \"field\": \"test field\"}]}").getBytes());
-        try {
-            final Scan s = r.readValue(content);
-            if(s == null) {
-                LOG.error("FAILED with NULL");
-                return false;
-            }
-            else {
+            // Simulation of location data stream
+            InputStream content = new ByteArrayInputStream(new String("{ \"findings\" : [{ \"uniqueId\": 0, \"field\": \"test field\"}]}").getBytes());
+            try {
+                final Scan s = r.readValue(content);
                 executor.submit(() -> {
                     try {
                         Thread.sleep(2500);
-                        for(Finding f : s.getFindings()) {
+                        for (Finding f : s.getFindings()) {
                             Vulnerability v = new Vulnerability();
                             v.setCustomAttributeValue("Field", f.getField());
                             v.setInstanceId(f.getUniqueId());
-                            v.setScanId(event.getScanId());
-                            publisher.publish(v);
+                            v.setScanId(scanSubmission.getScanId());
+                            publisher.send(v);
                         }
-                        LOG.info("Processed " + event.getScanId());
+                        LOG.info("Processed " + scanSubmission.getScanId());
                     } catch (InterruptedException e) {
                         LOG.warn("Interrupted!");
                     }
 
                 });
-                return true;
+            } catch (IOException e) {
+                LOG.error("FAILED because of my EX", e);
+            } finally {
+                try {
+                    if (content != null) content.close();
+                } catch (IOException e) {
+                }
             }
-        } catch (IOException  e) {
-            LOG.error("FAILED because of my EX", e);
-            return false;
-        } finally {
-            try {
-                content.close();
-            } catch (IOException ignored) { }
         }
+    }
+
+    @Override
+    public void setPublisher(PluginPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @Override
+    public boolean acceptScan(AcceptScan event) {
+        return true;
+    }
+
+    @Override
+    public com.fortify.plugin.result.Scan getScan() {
+        com.fortify.plugin.result.Scan scan = new com.fortify.plugin.result.Scan();
+        scan.setScanDate(new Date());
+        scan.setEngineVersion("1");
+        return scan;
+
     }
 }
