64ceee37113ba05e5e0a7cfa081ab65a84f1a7e9
Petr Svoboda <psvoboda@hpe.com>
2016-12-22 13:51:45 +0100

    Add scanDate to sample-parser scan format

--- src/main/java/com/thirdparty/scan/Scan.java
+++ src/main/java/com/thirdparty/scan/Scan.java
@@ -1,7 +1,21 @@
 package com.thirdparty.scan;
 
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.databind.util.Converter;
 
+import java.time.DateTimeException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalQueries;
+import java.util.Date;
+import java.util.IllegalFormatException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -13,6 +27,7 @@ public class Scan {
     
     private List<Finding> findings = new LinkedList<>();
 
+    private Date scanDate;
 
     public List<Finding> getFindings() {
         return findings;
@@ -20,5 +35,50 @@ public class Scan {
 
     public void setFindings(List<Finding> findings) {
         this.findings = findings;
+    }
+
+    public Date getScanDate() {
+        return scanDate;
+    }
+
+    @JsonDeserialize(converter = DateConverter.class)
+    public void setScanDate(final Date scanDate) {
+        this.scanDate = scanDate;
+    }
+
+    private static class DateConverter implements Converter<String, Date> {
+        private static final DateTimeFormatter[] DATE_TIME_FORMATTERS = {DateTimeFormatter.ISO_DATE_TIME};
+
+        @Override
+        public Date convert(final String dateStr) {
+            for (final DateTimeFormatter formatter : DATE_TIME_FORMATTERS) {
+                final TemporalAccessor temporalAccessor;
+                try {
+                    temporalAccessor = formatter.parse(dateStr);
+                } catch (final DateTimeException e) {
+                    // try next parser
+                    continue;
+                }
+                final Instant instant;
+                if (temporalAccessor.query(TemporalQueries.offset()) != null) {
+                    instant = OffsetDateTime.from(temporalAccessor).toInstant();
+                } else {
+                    instant = LocalDateTime.from(temporalAccessor).toInstant(ZoneOffset.UTC);
+                }
+                return Date.from(instant);
+            }
+            // no parser worked
+            throw new IllegalArgumentException("Unsupported date format: " + dateStr);
+        }
+
+        @Override
+        public JavaType getInputType(final TypeFactory typeFactory) {
+            return typeFactory.constructType(String.class);
+        }
+
+        @Override
+        public JavaType getOutputType(final TypeFactory typeFactory) {
+            return typeFactory.constructType(Date.class);
+        }
     }
 }
